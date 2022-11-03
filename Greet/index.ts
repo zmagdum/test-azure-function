c63228ea13738e2269454b986aadb278dd99e857
Martijn Storck <martijn@storck.io>
2022-11-03 21:18:55 +0100

    Remove leftover log

--- Greet/index.ts
+++ Greet/index.ts
@@ -4,7 +4,6 @@ const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
-  console.log(context);
   context.log("HTTP trigger function processed a request.");
   const name = req.query.name || (req.body && req.body.name);
   const responseMessage = name
